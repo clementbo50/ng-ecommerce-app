4e461e91f1f2bdf0dc9e73eca11421b4dff9ec77
clementbo <clementbo50@gmail.com>
2024-07-09 21:04:34 +0200

    Fin-module-7 les routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProductsCartComponent } from './products-cart/products-cart.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'boutique', component: ProductsCartComponent },
+    { path: 'add-product', component: AddProductComponent },
+    { path: '', redirectTo: 'boutique', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+];
